147dab0b63d342f9472ed2c048daa91f4fe704b1
ErickD70 <78987375+ErickD70@users.noreply.github.com>
2021-10-09 23:47:07 -0600

    .

---
+++ Escuela/Dominio/Erollement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escuela.Dominio
+{
+   public enum Garde { A, B, C, D,}
+    public class Erollement
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int ErollementID { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public Garde? Grade { get; set; }
+
+        public Course Course {get; set;}
+
+        public Student Student { get; set; }
+    }
+}
---
+++ Escuela/Dominio/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Escuela.Dominio
+{
+
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstMidName { get; set; }
+
+        public DateTime Enrrollments { get; set; }
+
+        public ICollection<Erollement> erollements { get; set; }
+    }
+}
